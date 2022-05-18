c8c8d3e7a7c89449be0fd2c1dcee415463c385a5
dickymuliafiqri <santrialanwari@gmail.com>
2022-05-18 07:52:19 +0700

    set config

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 export default {
-  name: "Friend", // actual name of the recipient (Mandatory)
+  name: "Damara", // actual name of the recipient (Mandatory)
 
-  nickname: "Buddy", // nickname(optional)
+  nickname: "Meila", // nickname(optional)
 
-  pic: "", // image url of recipients (Mandatory)
+  pic: "https://telegra.ph/file/01c47c01b279c3e3383ea.jpg", // image url of recipients (Mandatory)
 
   showScrollMsg: true, // set to false if you do not want the scrolling message
 
